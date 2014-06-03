47a2b8fd94c57a74ce4d6cf4b9557598bcd41123
lucis1986 <lucis1986@hotmail.com>
2014-06-03 13:21:02 +0800

    upload

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -3,6 +3,6 @@
 <head>
     <title><?php echo $title ?> - CodeIgniter 2 Tutorial</title>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-    <link rel="stylesheet" href="/codeigniter_2.1.4/css/core.css" type="text/css">
+    <link rel="stylesheet" href="/css/core.css" type="text/css">
 <body>
 
